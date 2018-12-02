d23da79146abb7f7a2cf7c039c63e23a0fb312f8
Filipe Lima <filipe.1992@gmail.com>
2018-12-02 21:20:52 -0200

    Identando codigo

--- pages/enfermeiro.php
+++ pages/enfermeiro.php
@@ -57,7 +57,7 @@ session_start();
         num=num.replace(/\D/g,"")
         return num
       }
-    </script>
+  </script>
 
   <!-- Bootstrap Core CSS -->
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -77,8 +77,7 @@ session_start();
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
       <![endif]-->
-
-    </head>
+</head>
 
     <body>
 
